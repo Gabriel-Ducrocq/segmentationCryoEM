1138a04b466c3a2cdc524e4b7b5670fd826a5ac1
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2022-11-10 16:27:27 +0100

    again

--- mlp.py
+++ mlp.py
@@ -8,14 +8,14 @@ class MLP(nn.Module):
         self.flatten = nn.Flatten()
         self.num_hidden_layers = num_hidden_layers
         ##Using leaky RELU !
-        self.input_layer = nn.Sequential(nn.Linear(in_dim, intermediate_dim), nn.LeakyReLU())
-        self.input_layer.to(device)
-        self.output_layer = nn.Sequential(nn.Linear(intermediate_dim, out_dim))
-        self.output_layer.to(device)
-        list_intermediate = [nn.Sequential(nn.Linear(intermediate_dim, intermediate_dim), nn.LeakyReLU())
+        self.input_layer = nn.Sequential(nn.Linear(in_dim, intermediate_dim, device=device), nn.LeakyReLU())
+        #self.input_layer.to(device)
+        self.output_layer = nn.Sequential(nn.Linear(intermediate_dim, out_dim, device=device))
+        #self.output_layer.to(device)
+        list_intermediate = [nn.Sequential(nn.Linear(intermediate_dim, intermediate_dim, device=device), nn.LeakyReLU())
                              for _ in range(num_hidden_layers)]
         self.linear_relu_stack = nn.Sequential(*[layer for layer in list_intermediate])
-        self.linear_relu_stack.to(device)
+        #self.linear_relu_stack.to(device)
 
     def forward(self, x):
         #x = self.flatten(x)
