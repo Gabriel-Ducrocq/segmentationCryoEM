optimizing only RMSD, with gradient on parameters and latent variables. RMSD drops to 2 and correct mask but not 0/1 values

main.py
@@ -11,7 +11,7 @@ import torchvision
 
 writer = SummaryWriter()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-batch_size = 500
+batch_size = 2000
 N_domains = 3
 latent_dim = 3*N_domains
 num_nodes = 1510
@@ -27,7 +27,7 @@ test_set_size = int(dataset_size/10)
 
 def train_loop(network, absolute_positions, nodes_features, edge_indexes, edges_features, latent_variables,
                generate_dataset=True, dataset_path="data/"):
-    optimizer = torch.optim.Adam(network.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
     #optimizer = torch.optim.SGD(network.parameters(), lr=5)
     #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08, verbose=False)
     #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
@@ -58,11 +58,11 @@ def train_loop(network, absolute_positions, nodes_features, edge_indexes, edges_
     #indexes = torch.linspace(0, 90000, steps=1, dtype=torch.long)
     indexes = torch.tensor(np.array(range(90000)))
     for epoch in range(1000):
-        epoch_loss = torch.empty(180)
+        epoch_loss = torch.empty(45)
         indexesDataLoader = DataLoader(indexes, batch_size=batch_size, shuffle=True)
-        for i in range(180):
+        for i in range(45):
             print("epoch:", epoch)
-            print(i/180)
+            print(i/45)
             print(network.multiply_windows_weights())
             ind = next(iter(indexesDataLoader))
             #latent_vars_normed = (latent_vars - avg)/std
@@ -74,7 +74,7 @@ def train_loop(network, absolute_positions, nodes_features, edge_indexes, edges_
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            k = np.random.randint(0, 500)
+            k = np.random.randint(0, 2000)
             epoch_loss[i] = loss
             print(translations[k, :, :])
             #print(true_deformation[k, :, :]**3)