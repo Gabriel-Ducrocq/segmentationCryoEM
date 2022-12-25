2accd473a8f4fae5e80c0b4255546534cb9f1b83
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2022-12-25 23:02:44 +0100

    trying remotely

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def train_loop(network, absolute_positions, renderer, generate_dataset=True, dat
 
     if generate_dataset:
         #true_deformations = 5*torch.randn((dataset_size,3*N_input_domains))
-        conformation1 = torch.tensor(np.array([[-7, -7, 0, 0, 0, 0, 7, 7, 0, 0, 0, 0]]), dtype=torch.float32)
+        conformation1 = torch.tensor(np.array([[-7, -7, 0, 0, 0, 0, 5, 5, 0, 0, 0, 0]]), dtype=torch.float32)
         conformation2 = torch.tensor(np.array([7, -7, 0,  0, 0, 0, 0, 0, 0, 0, 0, 0]), dtype=torch.float32)
         conformation1 = torch.broadcast_to(conformation1, (25000, 12))
         conformation2 = torch.broadcast_to(conformation2, (25000, 12))
