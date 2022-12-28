rotation prediction works without bug, check everything is alright tho

main.py
@@ -198,7 +198,7 @@ def experiment(graph_file="data/features.npy"):
     local_frame = torch.tensor(features["local_frame"])
     local_frame = local_frame.to(device)
 
-    translation_mlp = MLP(latent_dim + 4, 3*N_input_domains, 350, device, num_hidden_layers=2)
+    translation_mlp = MLP(latent_dim + 4, 2*3*N_input_domains, 350, device, num_hidden_layers=2)
     encoder_mlp = MLP(N_pixels, latent_dim*2, 1024, device, num_hidden_layers=4)
 
     pixels_x = np.linspace(-150, 150, num=64).reshape(1, -1)