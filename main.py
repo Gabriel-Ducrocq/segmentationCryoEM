76ca4273e4dc6f4e1e5532dd9299888b339a0c49
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2023-01-06 17:36:35 +0100

    trying with lr=0.01

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ test_set_size = int(dataset_size/10)
 
 print("Is cuda available ?", torch.cuda.is_available())
 
-def train_loop(network, absolute_positions, renderer, local_frame, generate_dataset=True, dataset_path="data/test/"):
+def train_loop(network, absolute_positions, renderer, local_frame, generate_dataset=True, dataset_path="data/testlr01/"):
     optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=300)
     all_losses = []
