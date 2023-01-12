removing transposition of pytorch3d rotation matrix, since it was finally in the correct order

main.py
@@ -86,11 +86,11 @@ def train_loop(network, absolute_positions, renderer, local_frame, generate_data
 
 
         training_set = true_deformations.to(device)
-        rotation_matrices = torch.transpose(rotation_matrices, dim0=-2, dim1=-1)
+        #rotation_matrices = torch.transpose(rotation_matrices, dim0=-2, dim1=-1)
         training_rotations_matrices = rotation_matrices.to(device)
         training_rotations_angles = rotation_angles.to(device)
         training_rotations_axis = rotation_axis.to(device)
-        conformation_rotation_matrix = torch.transpose(conformation_rotation_matrix, dim0=-2, dim1=-1)
+        #conformation_rotation_matrix = torch.transpose(conformation_rotation_matrix, dim0=-2, dim1=-1)
         training_conformation_rotation_matrix = conformation_rotation_matrix.to(device)
 
 

network.py
@@ -119,7 +119,7 @@ class Net(torch.nn.Module):
 
         mask_rotation_matrix_per_domain_per_residue = axis_angle_to_matrix(mask_rotation_per_domains_axis_angle)
         #Transposed here because pytorch3d has right matrix multiplication convention.
-        mask_rotation_matrix_per_domain_per_residue = torch.transpose(mask_rotation_matrix_per_domain_per_residue, dim0=-2, dim1=-1)
+        #mask_rotation_matrix_per_domain_per_residue = torch.transpose(mask_rotation_matrix_per_domain_per_residue, dim0=-2, dim1=-1)
         overall_rotation_matrices = torch.zeros((self.batch_size, self.N_residues,3,3), device=self.device)
         overall_rotation_matrices[:, :, 0, 0] = 1
         overall_rotation_matrices[:, :, 1, 1] = 1