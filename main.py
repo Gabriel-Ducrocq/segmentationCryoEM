d2663a9bf2bb5433857db009d3909adc2f90c57a
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2023-03-15 11:04:05 +0100

    trying with lr = 0.0003

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ print("Is cuda available ?", torch.cuda.is_available())
 
 def train_loop(network, absolute_positions, renderer, local_frame, generate_dataset=True,
                dataset_path="data/vaeContinuousNoisy/"):
-    optimizer = torch.optim.Adam(network.parameters(), lr=0.00003)
+    optimizer = torch.optim.Adam(network.parameters(), lr=0.0003)
     #optimizer = torch.optim.Adam(network.parameters(), lr=0.003)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=300)
     #scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=500, T_mult=1, eta_min=0.00003)
