39243ee936d1dc9d5d079dab19ac623c9e260d5e
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2023-07-07 13:49:09 +0200

    commit before checkout

--- analysis.py
+++ analysis.py
@@ -4,18 +4,19 @@ import numpy as np
 import time
 import utils
 from imageRenderer import Renderer
+from pytorch3d.transforms import quaternion_to_axis_angle
 
 #dataset_path="data/vaeContinuousCTFNoisyBiModalAngle100kEncoder/"
-dataset_path="../VAEProtein/data/vaeContinuousCTFNoisyBiModalAngle10kEncoderOldFashioned/"
+dataset_path="../VAEProtein/data/vaeContinuousMD/"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 batch_size = 100
 #This represent the number of true domains
-N_domains = 3
+N_domains = 6
 N_pixels = 240*240
 #This represents the number of domain we think there are
-N_input_domains = 4
-latent_dim = 9
-num_nodes = 1510
+N_input_domains = 6
+latent_dim =
+num_nodes = 1006
 cutoff1 = 300
 cutoff2 = 1353
 K_nearest_neighbors = 30
@@ -38,21 +39,23 @@ pixels_x = np.linspace(-150, 150, num=64).reshape(1, -1)
 pixels_y = np.linspace(-150, 150, num=64).reshape(1, -1)
 renderer = Renderer(pixels_x, pixels_y, std=1, device=device)
 #model_path = "data/vaeContinuousCTFNoisyBiModalAngle100kEncoder/full_model"
-model_path = "../VAEProtein/data/vaeContinuousCTFNoisyBiModalAngle10kEncoderOldFashioned/network"
+model_path = "../VAEProtein/data/vaeContinuousMD/full_model2141"
 model = torch.load(model_path, map_location=torch.device(device))
 
 
-training_set = torch.load(dataset_path + "training_set", map_location=torch.device(device)).to(device)
-training_rotations_angles = torch.load(dataset_path + "training_rotations_angles", map_location=torch.device(device)).to(device)
-training_rotations_axis = torch.load(dataset_path + "training_rotations_axis", map_location=torch.device(device)).to(device)
+#training_set = torch.load(dataset_path + "training_set", map_location=torch.device(device)).to(device)
+#training_rotations_angles = torch.load(dataset_path + "training_rotations_angles", map_location=torch.device(device)).to(device)
+#training_rotations_axis = torch.load(dataset_path + "training_rotations_axis", map_location=torch.device(device)).to(device)
 training_rotations_matrices = torch.load(dataset_path + "training_rotations_matrices", map_location=torch.device(device)).to(device)
-training_conformation_rotation_matrix = torch.load(dataset_path + "training_conformation_rotation_matrices", map_location=torch.device(device))
-
+#training_conformation_rotation_matrix = torch.load(dataset_path + "training_conformation_rotation_matrices", map_location=torch.device(device))
+images = torch.load(dataset_path + "continuousConformationDataSet", map_location=torch.device(device))
 #print("SHOULD WE USE ENCODER:", model.use_encoder)
-print("DATASET SIZE:", training_set.shape)
+#print("DATASET SIZE:", training_set.shape)
 training_indexes = torch.tensor(np.array(range(10000)))
 all_latent_distrib = []
 all_indexes = []
+all_rot = []
+all_translations = []
 for epoch in range(0, 1):
     epoch_loss = torch.empty(1)
     # data_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True)
@@ -65,25 +68,24 @@ for epoch in range(0, 1):
         batch_indexes = next(data_loader)
         print(batch_indexes)
         ##Getting the batch translations, rotations and corresponding rotation matrices
-        batch_data = training_set[batch_indexes]
-        batch_rotations_angles = training_rotations_angles[batch_indexes]
-        batch_rotations_axis = training_rotations_axis[batch_indexes]
-        batch_rotation_matrices = training_rotations_matrices[batch_indexes]
-        batch_data_for_deform = torch.reshape(batch_data, (batch_size, N_input_domains, 3))
-        batch_conformation_rotation_matrices = training_conformation_rotation_matrix[batch_indexes]
-        ## Deforming the structure for each batch data point
-        deformed_structures = utils.deform_structure(absolute_positions, cutoff1, cutoff2, batch_data_for_deform,
-                                                     batch_conformation_rotation_matrices, local_frame,
-                                                     relative_positions,
-                                                     1510, device)
+        batch_images = torch.flatten(images[batch_indexes], start_dim=1, end_dim=2)
+        latent_distrib = model.encoder.forward(batch_images)
+        transforms = model.decoder.forward(latent_distrib[:, :latent_dim])
+        transforms = torch.reshape(transforms, (batch_size, N_input_domains, 2 * 3))
+        scalars_per_domain = transforms[:, :, :3]
 
-        print("Deformed")
-        ## We then rotate the structure and project them on the x-y plane.
-        deformed_images = renderer.compute_x_y_values_all_atoms(deformed_structures, batch_rotation_matrices)
-        latent_distrib = model.encoder.forward(deformed_images)
+        ones = torch.ones(size=(batch_size, N_input_domains, 1), device=device)
+        quaternions_per_domain = torch.cat([ones, transforms[:, :, 3:]], dim=-1)
+        rotation_per_domains_axis_angle = quaternion_to_axis_angle(quaternions_per_domain)
+        translation_vectors = torch.matmul(scalars_per_domain, local_frame)
+
+        all_rot.append(rotation_per_domains_axis_angle.detach().cpu().numpy())
+        all_translations.append(translation_vectors.detach().cpu().numpy())
         all_latent_distrib.append(latent_distrib.detach().cpu().numpy())
         all_indexes.append(batch_indexes.detach().cpu().numpy())
 
-
 np.save(dataset_path + "latent_distrib.npy", np.array(all_latent_distrib))
 np.save(dataset_path + "indexes.npy", np.array(all_indexes))
+
+np.save(dataset_path + "all_rotations.npy", np.array(all_rot))
+np.save(dataset_path + "all_translations", np.array(all_translations))
